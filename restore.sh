5cad9b056e0ded9e006166231d720dbf91e44106
Fabian Norman <FabianN@gmail.com>
2011-03-16 09:57:24 -0700

    Added a check to see if var $2 is a number.

--- restore.sh
+++ restore.sh
@@ -1,24 +1,35 @@
 # Mail restore script made by Fabian Norman (FabianN@gmail.com)
-# Made during Employment at UO IS Help Desk for us of UO Staff,
+# Made during Employment at UO IS Help Desk for use of UO Staff,
 # faculty, and other members. This script is only designed to function
 # on UO systems to restore UO mail. Using this script in any manner other
 # than directed is not advisable.
 
+# Change variables to lowercase
+echo $1 | tr '[:upper:]' '[:lower:]'
+
+# Checking 2nd var, is number?
+if [ $2 -eq $2 2> /dev/null ]; then
+echo $2 is a number
+else
+echo "You entered $2 as time-frame. This is not a number. Please enter a number."
+exit 0
+fi
+
 # Go to home directory
 cd ~/
 
-
+# Lock the task so that script is unable to run multiple times.
 if [[ -e mailbackup.lock ]]; then
 	echo "Backup already in progress.  Aborting..."
 	echo "If this message is in error please remove the mailbackup.lock file located in your home directory."
-	exit 1
+	exit 0
 else
 	touch mailbackup.lock
 fi
 
 #Does the .oldmail dir exist?
 #To do: if .oldmail exists, put it into a sub-folder? (prompt as a question)
-####### Also, ask if user wants to empy/clear oldmail (bad attempt?)
+####### Also, ask if user wants to empy/clear oldmail (they had a failed/bad attempt?)
 if [[ ! -d Maildir/.oldmail  ]]; then
 	if ! mkdir -p Maildir/.oldmail; then
 		echo "Backup path ~/Maildir/.oldmail does not exist and I could not create the directory!"
@@ -27,25 +38,29 @@ if [[ ! -d Maildir/.oldmail  ]]; then
 fi
 
 
-#Check the varables, are they clean?
-
-#Checking first setting, should be weekly 
+#Checking the time-range varable, are they clean?
 
 if [[ "$1" =~ "hourly" ]] ; then
 	echo "Picking from the hourly group..."
+
+
 	sleep 3
 
 elif [[ "$1" =~ "nightly" ]] ; then
 	echo "Picking from the nightly group..."
 	sleep 3
+
 elif [[ "$1" =~ "weekly" ]] ; then
 	echo "Picking from the weekly group..."
 	sleep 3
+
 elif [[ -z $1 ]] ; then
 	echo "Time range not set. Please pick a time-range. Choose hourly, weekly, nightly."
 	exit 0
+
 else
-	echo "Time range not set correctly. Please pick a time range spelled exactyl as so: hourly, weekly, nightly."
+	echo "Time range not set correctly. You entered $1."
+	echo "Please pick a time range spelled exactyl as so: hourly, weekly, nightly."
 	exit 0
 
 fi
@@ -58,6 +73,7 @@ fi
 #rsync -a --progress ~/Maildir/.snapshot/hourly.2/cur/ ~/Maildir/.oldmail/cur/
 
 
+rm mailbackup.lock
 
 #Ending message
 echo "Mail backup has been completed. To get the mail to display in your inbox you will have to manually add the oldmail folder to webmail. Follow these directions to add the folder to Webmail."
