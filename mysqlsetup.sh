4e097d335a71be03a4a25d0880bd110bd30db804
Fabian Norman <FabianN@gmail.com>
2011-04-05 11:23:24 -0700

    Added mysqlsetup.sh

---
+++ mysqlsetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# MySQL setup script made by Fabian Norman (fnorman@uoregon.edu, FabianN@gmail.com)
+# Made during Employment at UO IS Help Desk for use of UO Staff,
+# faculty, and other members. This script is only designed to function
+# on UO systems to setup the MySQL database under the user's account.
+# Using this script in any manner other than directed is not advisable.
+
+
