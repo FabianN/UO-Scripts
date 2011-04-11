845cd911917fcd5242a82e6d84fa49cce07c6684
Fabian Norman <FabianN@gmail.com>
2011-04-11 12:28:17 -0700

    Adjusted the header.

--- restoremail.sh
+++ restoremail.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# UO Mail Snapshot Restore v 1.1
+########################################################################
 # Mail restore script made by Fabian Norman (fnorman@uoregon.edu, FabianN@gmail.com)
 # Made during Employment at UO IS Help Desk for use of UO Staff,
 # faculty, and other members. This script is only designed to function
 # on UO systems to restore UO mail. Using this script in any manner other
 # than directed is not advisable.
-# UO Mail Snapshot Restore v 1.1
+########################################################################
+
 
 echo ""
 echo "=========================="
