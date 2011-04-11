08a1977a38f81909bfeadb61beeff7bb334181a9
Fabian Norman <FabianN@gmail.com>
2011-04-11 08:53:25 -0700

    Added port number chooser, credit goes to Jeremy Blanchard(http://auzigog.com/2009/02/01/easy-website-on-your-university-of-oregon-web-space/)

--- mysqlsetup.sh
+++ mysqlsetup.sh
@@ -8,3 +8,22 @@
 # Change to home dir
 cd ~/
 
+#Gather information
+echo "Welcome to the MySQL setup script. This script will configure the MySQL service on your user account"
+echo "Before we start I will need to have what you would like to set the MySQL root password to. It will be your responsibility to keep this password safe. If you lose it there is no garentee that the password can be recovered or reset."
+read -p "root password: " PASSWORD
+
+echo "I will now create a lock file to prevent re-running this script. Please remove mysql.lock located in your home directory if you need to re-run this script."
+touch mysql.lock
+
+## Get port number
+
+PORT=0
+PORTOUTPUT=1
+while [ $PORTOUTPUT ]
+do
+	RANDNUM=$[ $RANDOM % 1000 + 5000 ] # generates a number between 5000 and 6000
+	PORT=$RANDNUM
+	PORTOUTPUT=`netstat -lt | grep $PORT`
+done
+echo "// For reference, the port number that MySQL is running on is: $PORT"
