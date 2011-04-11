6091a9d838814a00e4e226c0fb915db58f454fcd
Fabian Norman <FabianN@gmail.com>
2011-04-11 09:23:17 -0700

    Created the password creation/check loop.

--- mysqlsetup.sh
+++ mysqlsetup.sh
@@ -11,8 +11,25 @@ cd ~/
 #Gather information
 echo "Welcome to the MySQL setup script. This script will configure the MySQL service on your user account"
 echo "Before we start I will need to have what you would like to set the MySQL root password to. It will be your responsibility to keep this password safe. If you lose it there is no garentee that the password can be recovered or reset."
-read -p "root password: " PASSWORD
+PWD=0
+until [[ $PWD == 1 ]]; do
+	read -p "root password: " PASSWORDA
+	echo "Please re-enter the password to confirm"
+	read -p "Password Confirmation: " PASSWORDB
+	if [[ $PASSWORDA == $PASSWORDB ]]; then
+	echo "Passwords match!"
+	PWD=1
+	else
+	echo "Passwords do not match!"
+	echo "Please re-try entering the passwords."
+	echo "------------------"
+	echo " "
+	fi
+done
 
+echo "Exited loop"
+
+: '
 echo "I will now create a lock file to prevent re-running this script. Please remove mysql.lock located in your home directory if you need to re-run this script."
 touch mysql.lock
 
@@ -27,3 +44,4 @@ do
 	PORTOUTPUT=`netstat -lt | grep $PORT`
 done
 echo "// For reference, the port number that MySQL is running on is: $PORT"
+'
