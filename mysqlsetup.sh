947a45092f66570666beee904c223d8b111c246f
Fabian Norman <FabianN@gmail.com>
2011-04-11 11:09:12 -0700

    Cleaned up the output of the script.

--- mysqlsetup.sh
+++ mysqlsetup.sh
@@ -9,17 +9,18 @@
 cd ~/
 
 #Gather information
-echo "Welcome to the MySQL setup script. This script will configure the MySQL service on your user account"
+echo "Welcome to the MySQL setup script. This script will configure the MySQL service on your user account."
 echo "Before I am able to setup MySQL for you I will need to collect some information."
 echo "First I will need to have what you would like to set the MySQL root password to."
-echo "It will be your responsibility to keep this password safe. If you lose it there is no garentee that the password can be recovered or reset."
+echo "It will be your responsibility manage the password. If you lose it there is no garentee that the password can be recovered or reset."
 echo "------------------"
 echo " "
-
+#Make password
 PWD=0
 until [[ $PWD == 1 ]]; do #Re-run until PWD = 1
 	#Ask for the password twice
 	read -p "root password: " PASSWORDA
+	echo " "
 	echo "Please re-enter the password to confirm"
 	read -p "Password Confirmation: " PASSWORDB
 	#Check if passwords match	
@@ -39,6 +40,7 @@ until [[ $PWD == 1 ]]; do #Re-run until PWD = 1
 done
 
 echo "Now I will find an un-used port number to run MySQL off of."
+sleep 1
 #Get port number
 PORT=0
 PORTOUTPUT=1
@@ -49,22 +51,28 @@ do
 	PORTOUTPUT=`netstat -lt | grep $PORT`
 done
 echo "MySQL will run off of the port $PORT."
-echo "You may need the port number in the future so be sure to note it down."
 echo "------------------"
+sleep 2
 echo " "
 
 #Review information collected
 echo "I have collected the needed information. Please review the information listed below :"
+sleep 1
 echo "=================="
 echo "Password : $PASSWORDA"
 echo "Port #   : $PORT"
+echo "=================="
+echo "If you loose this information there is no garantee it can be recovered. Ensure that you note this information down"
+echo " "
+echo "If this information is correct please continue, otherwise cancel the script."
 
-read -p "Setup MySQL now? (y/n)" REPLY
+#Info correct?
+read -p "Continue? (y/n)" REPLY
 if [[ "$REPLY" == "n" ]]; then
 	echo "Exiting the script..."
 	exit 0
 fi
+echo " "
 echo "I will now create a lock file to prevent re-running this script. Please remove mysql.lock located in your home directory if you need to re-run this script."
-#touch mysql.lock
-
+touch mysql.lock
 
