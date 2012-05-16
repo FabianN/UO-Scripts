80c161a6811cce185d395329c2fecf7af8ad96b9
Fabian Norman <FabianN@gmail.com>
2012-05-16 12:50:28 -0700

    Fixed formatting in header and added a license.

--- restoremail.sh
+++ restoremail.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 # UO Mail Snapshot Restore v 1.1
 ########################################################################
-# Mail restore script made by Fabian Norman (fnorman AT uoregon DOT edu, FabianN AT gmail DOT com)
+# Mail restore script made by Fabian Norman
+# (fnorman AT uoregon DOT edu, FabianN AT gmail DOT com)
 # Made during Employment at UO IS Help Desk for use of UO Staff,
 # faculty, and other members. This script is only designed to function
 # on UO systems to restore UO mail. Using this script in any manner other
 # than directed is not advisable.
+#
+# This script is licensed under the 'Do what you want to' license.
+#
+#  DO WHAT YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2012 Fabian Norman <fabiann AT gmail DOT com>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT YOU WANT TO. 
+#
 ########################################################################
 
 
