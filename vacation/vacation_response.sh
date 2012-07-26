ccd97a8205d3d7565846dc3702e790e0fc3941b0
Fabian Norman <fabiann@gmail.com>
2012-07-26 14:21:34 -0700

    Made the output text more clear and seperated from the rest.

--- vacation/vacation_response.sh
+++ vacation/vacation_response.sh
@@ -30,26 +30,27 @@
 
 case "$1" in
 	"on"|*)
-		
-		echo "It looks like you want to enable your vacation response."
-		echo "A text editor program is going to open up so you can create your vacation response. Here are important controls you need to note:"
-		echo "--"
-		echo "1) Ctrl + O = Save changes to file."
-		echo "2) Ctrl + X = Exit editor."
-		echo "3) Ctrl + G = Additional help"
-		echo "--"
-		read -sn 1 -p 'Press any key when you are ready to edit your vacation response.';echo
+		echo "============================================"
+		echo "= It looks like you want to enable your vacation response."
+		echo "= A text editor program is going to open up so you can create your vacation response. Here are important controls you need to note:"
+		echo "=--"
+		echo "= 1) Ctrl + O = Save changes to file."
+		echo "= 2) Ctrl + X = Exit editor."
+		echo "= 3) Ctrl + G = Additional help"
+		echo "=--"
+		read -sn 1 -p '= Press any key when you are ready to edit your vacation response.';echo
 		nano ~/test/.vacation.msg
 		cp -f /usr/local/share/vacation/rc.vacation ~/test/rc.vacation
 		echo "INCLUDERC=\$HOME/rc.vacation" > ~/test/.procmailrc
-		echo "============================================'
-		echo "The vacation responder has been created."
-		echo "If you want to remove the vacation responder using this script you can run this script like so:"
-		echo "./vacation_response.sh off"
-
+		echo "============================================"
+		echo "= The vacation responder has been created."
+		echo "= If you want to remove the vacation responder using this script you can run this script like so:"
+		echo "= ./vacation_response.sh off"
+		echo "============================================"
 
 	;;
 	"off")
+		
 
 	;;
 esac
