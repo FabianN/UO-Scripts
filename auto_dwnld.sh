580ced5d16633545fd3e3e32290881ae5da9a522
Fabian Norman <fabiann@gmail.com>
2012-09-12 11:07:45 -0700

    Automatic download script.

---
+++ auto_dwnld.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --no-check-certificate -O ~/restoremail.sh https://raw.github.com/FabianN/UO-Scripts/master/email_restore/restoremail.sh
+wget --no-check-certificate -O ~/phpcreate.sh https://raw.github.com/FabianN/UO-Scripts/master/site_setup/phpcreate.sh
+wget --no-check-certificate -O ~/mysqlsetup.sh https://raw.github.com/FabianN/UO-Scripts/master/site_setup/mysqlsetup.sh
